04e9d0069f232a12abe2fe7f76651a1c5ca4dfdd
David <depch47@gmail.com>
2022-06-16 11:31:38 -0500

    ADD---create provider usecase

--- src/main/java/com/sofka/hardwarepublisher/usecase/CreateProviderUseCase.java
+++ src/main/java/com/sofka/hardwarepublisher/usecase/CreateProviderUseCase.java
@@ -1,13 +1,32 @@
 package com.sofka.hardwarepublisher.usecase;
 
+import com.sofka.hardwarepublisher.domain.Provider;
 import com.sofka.hardwarepublisher.infra.dto.ProviderDTO;
+import com.sofka.hardwarepublisher.infra.mapper.ProviderMapper;
+import com.sofka.hardwarepublisher.infra.repository.ProviderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
 
 @Service
 public class CreateProviderUseCase {
 
+    private final ProviderMapper mapper;
+
+    private final ProviderRepository repository;
+
+    public CreateProviderUseCase(ProviderMapper mapper, ProviderRepository repository) {
+        this.mapper = mapper;
+        this.repository = repository;
+    }
+
     public Mono<ProviderDTO> apply(ProviderDTO providerDTO) {
-        return null;
+        Provider provider = mapper.fromDTOToDomain(providerDTO);
+        provider.setAssociationDate(LocalDate.now().toString());
+
+        return repository.save(mapper.fromDomainToDocument(provider)).map(document -> mapper.fromDocumentToDTO(document));
+
     }
 }
